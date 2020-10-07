c28e3a86518a8857d633221363be8da0eb1e7039
edmobe <31488944+edmobe@users.noreply.github.com>
2020-10-06 18:57:02 -0600

    Update executable parsing to read its parameters

--- test.c
+++ test.c
@@ -2,8 +2,13 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-int main()
+int main(int argc, char *argv[])
 {
     printf("This is a test file\n");
+    for (int i = 1; i < argc; i++)
+    {
+        printf("Argument %d: %s\n", i, argv[i]);
+    }
+
     return 0;
 }
